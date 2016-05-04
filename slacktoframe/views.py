36017b9dbf603d1f5da9cba88b61371da97ba21c
Vladimir Nesic <v.nesic@hotmail.com>
2016-05-04 14:13:24 +0200

    frame/instance covered with tests and polished

--- slacktoframe/views.py
+++ slacktoframe/views.py
@@ -2,11 +2,12 @@ import os
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import Greeting
-from .slackcmd import execute_slack_slash_command
-from .frameinstance import start_frame_instance
+from .slackcmd import SlackCmdFrame
+from .crypto import FrameCypher, FrameCypherException
 
 # Create your views here.
 def index(request):
@@ -31,9 +32,16 @@ def db(request):
 
 
 def frame_instance_request(request, token):
-    return start_frame_instance(request, token)
+    try:
+        file_mapping, file_url = FrameCypher().decrypt(token)
+        return render(request, 'frame-instance.html', {'mapping': file_mapping, 'file_url': file_url})
+    except FrameCypherException, e:
+        return HttpResponseBadRequest('400 NOT FOUND {}, error = {}'.format(request.path, e.get_error()))
 
 
 @csrf_exempt
 def slack_slash_cmd_request(request, username):
-    return execute_slack_slash_command(request, username)
+    if request.POST.get('command', '') == '/frame':
+        return SlackCmdFrame.execute(request, username)
+    else:
+        return HttpResponseBadRequest('400 BAD REQUEST: Command {} unknown'.format(request.POST.get('command', '')))
