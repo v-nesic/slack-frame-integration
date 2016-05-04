cde3bc47b750ac8cb85d6c992dc30caef893b1c6
Vladimir Nesic <v.nesic@hotmail.com>
2016-05-04 14:26:56 +0200

    Remove demo project leftovers

--- slacktoframe/views.py
+++ slacktoframe/views.py
@@ -1,42 +1,9 @@
-import os
-
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.http import HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import Greeting
 from .slackcmd import SlackCmdFrame
 from .crypto import FrameCypher, FrameCypherException
-
-# Create your views here.
-def index(request):
-    times = int(os.environ.get('TIMES',3))
-    response = request.GET.get('q', 'Hello! ' * times)
-    return HttpResponse(response)
-    #r = requests.get('http://httpbin.org/status/418')
-    #print r.text
-    #return HttpResponse('<pre>' + r.text + '</pre>')
-    # return HttpResponse('Hello from Python!')
-    # return render(request, 'index.html')
-
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
-
-
-def frame_instance_request(request, token):
-    try:
-        file_mapping, file_url = FrameCypher().decrypt(token)
-        return render(request, 'frame-instance.html', {'mapping': file_mapping, 'file_url': file_url})
-    except FrameCypherException, e:
-        return HttpResponseBadRequest('400 NOT FOUND {}, error = {}'.format(request.path, e.get_error()))
 
 
 @csrf_exempt
@@ -45,3 +12,11 @@ def slack_slash_cmd_request(request, username):
         return SlackCmdFrame.execute(request, username)
     else:
         return HttpResponseBadRequest('400 BAD REQUEST: Command {} unknown'.format(request.POST.get('command', '')))
+
+
+def frame_instance_request(request, token):
+    try:
+        file_mapping, file_url = FrameCypher().decrypt(token)
+        return render(request, 'frame-instance.html', {'mapping': file_mapping, 'file_url': file_url})
+    except FrameCypherException, e:
+        return HttpResponseBadRequest('400 NOT FOUND {}, error = {}'.format(request.path, e.get_error()))
