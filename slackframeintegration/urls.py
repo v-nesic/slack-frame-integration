608eedf84997a652fefe7501702ace9c67434c57
Vladimir Nesic <v.nesic@hotmail.com>
2016-05-04 14:26:56 +0200

    Remove demo project leftovers

--- slackframeintegration/urls.py
+++ slackframeintegration/urls.py
@@ -7,13 +7,7 @@ admin.autodiscover()
 import slacktoframe.views
 
 
-# Examples:
-# url(r'^$', 'slackframeintegration.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
-
 urlpatterns = [
-    url(r'^$', slacktoframe.views.index, name='index'),
-    url(r'^db', slacktoframe.views.db, name='db'),
     url(r'^slack/([a-zA-Z_][0-9a-zA-Z\-_.]*)/slash-cmd', slacktoframe.views.slack_slash_cmd_request, name='slack-slash-cmd'),
     url(r'^frame/instance/([0-9a-zA-Z-_]+)/', slacktoframe.views.frame_instance_request, name='frame-instance'),
     url(r'^admin/', include(admin.site.urls)),
